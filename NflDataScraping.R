df8b18c00099986fa803fa5ab9f3e4bb43ca097f
Theotecles <45003847+Theotecles@users.noreply.github.com>
2019-08-29 18:10:46 -0400

    Add files via upload

---
+++ NflDataScraping.R
@@ -0,0 +1,12 @@
+# INSTALL NFLSCRAPR PACKAGE
+
+library(nflscrapR)
+
+# SCRAPE THE PAST 3 YEARS OF NFL PLAY BY PLAY DATA
+
+season_2018 <- season_play_by_play(2018)
+
+season_2017 <- season_play_by_play(2017)
+
+season_2016 <- season_play_by_play(2016)
+
