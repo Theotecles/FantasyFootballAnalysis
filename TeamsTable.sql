5f6fde2c7a230e5efe423a3ca92f079d865777d2
Theotecles <45003847+Theotecles@users.noreply.github.com>
2019-11-14 18:08:35 -0500

    Add files via upload

---
+++ TeamsTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE TEAMS (
+	TeamName varchar(255),
+	TeamAbbreviation varchar(255),
+	PrimaryColor varchar(255),
+	SecondaryColor varchar(255),
+	TertiaryColor varchar(255),
+	QuaternaryColor varchar(255),
+	Division varchar(255)
+	)
